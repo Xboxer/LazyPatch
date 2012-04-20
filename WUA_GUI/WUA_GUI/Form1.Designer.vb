55c40b5f4ec2d0816d684c31c5c9ee6025c677a2
Christoph Kaliske <christoph@kaliske.eu>
2012-04-20 11:15:14 +0200

    First

---
+++ WUA_GUI/WUA_GUI/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Host = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Updatestatus = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Installstatus = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Downloadforschritt = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.InstallationsforschrittAktuell = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.InstallationsforschrittGesamt = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button_Add = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.DownloadAndInstallUpdatesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Host, Me.Updatestatus, Me.Installstatus, Me.Downloadforschritt, Me.InstallationsforschrittAktuell, Me.InstallationsforschrittGesamt})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 83)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1316, 413)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Host
+        '
+        Me.Host.HeaderText = "Host"
+        Me.Host.Name = "Host"
+        '
+        'Updatestatus
+        '
+        Me.Updatestatus.HeaderText = "Updatestatus"
+        Me.Updatestatus.Name = "Updatestatus"
+        '
+        'Installstatus
+        '
+        Me.Installstatus.HeaderText = "Installstatus"
+        Me.Installstatus.Name = "Installstatus"
+        '
+        'Downloadforschritt
+        '
+        Me.Downloadforschritt.HeaderText = "Downloadforschritt"
+        Me.Downloadforschritt.Name = "Downloadforschritt"
+        '
+        'InstallationsforschrittAktuell
+        '
+        Me.InstallationsforschrittAktuell.HeaderText = "InstallationsforschrittAktuell"
+        Me.InstallationsforschrittAktuell.Name = "InstallationsforschrittAktuell"
+        '
+        'InstallationsforschrittGesamt
+        '
+        Me.InstallationsforschrittGesamt.HeaderText = "InstallationsforschrittGesamt"
+        Me.InstallationsforschrittGesamt.Name = "InstallationsforschrittGesamt"
+        '
+        'Button_Add
+        '
+        Me.Button_Add.Location = New System.Drawing.Point(18, 49)
+        Me.Button_Add.Name = "Button_Add"
+        Me.Button_Add.Size = New System.Drawing.Size(74, 25)
+        Me.Button_Add.TabIndex = 1
+        Me.Button_Add.Text = "Add"
+        Me.Button_Add.UseVisualStyleBackColor = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DownloadAndInstallUpdatesToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(229, 26)
+        '
+        'DownloadAndInstallUpdatesToolStripMenuItem
+        '
+        Me.DownloadAndInstallUpdatesToolStripMenuItem.Name = "DownloadAndInstallUpdatesToolStripMenuItem"
+        Me.DownloadAndInstallUpdatesToolStripMenuItem.Size = New System.Drawing.Size(228, 22)
+        Me.DownloadAndInstallUpdatesToolStripMenuItem.Text = "Download and Install Updates"
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1340, 508)
+        Me.Controls.Add(Me.Button_Add)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Host As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Updatestatus As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Installstatus As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Downloadforschritt As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents InstallationsforschrittAktuell As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents InstallationsforschrittGesamt As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Button_Add As System.Windows.Forms.Button
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Public WithEvents DownloadAndInstallUpdatesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ WUA_GUI/WUA_GUI/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Dim t As System.Threading.Thread
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_Add.Click
+        Dim temppc
+        temppc = InputBox("Rechnernamen eingeben", "Add PC", "")
+        DataGridView1.Rows.Add(temppc)
+    End Sub
+
+    Private Sub DataGridView1_CellMouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellMouseEventArgs) Handles DataGridView1.CellMouseUp
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+            ContextMenuStrip1.Show(Cursor.Position)
+        End If
+
+
+
+    End Sub
+
+    Private Sub DownloadAndInstallUpdatesToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DownloadAndInstallUpdatesToolStripMenuItem.Click
+        Dim cell As DataGridViewCell
+
+        For Each cell In DataGridView1.SelectedCells
+
+            Microsoft.VisualBasic.FileIO.FileSystem.CopyDirectory("C:\Update", "\\" & cell.Value.ToString & "\c$\Update")
+            Process.Start("psexec.exe", "\\" & cell.Value.ToString & " -s -h C:\Update\WUA_Client.exe")
+
+            Dim FS_updatestatus As New System.IO.FileStream("\\" & cell.Value.ToString & "\c$\Update\updatestatus.log", IO.FileMode.Open, IO.FileAccess.Read)
+            Dim FSR As New System.IO.StreamReader(FS_updatestatus)
+            Dim temp
+            While 1
+
+                temp = FSR.ReadLine
+
+            End While
+
+        Next
+
+
+    End Sub
+
+    Private Sub ContextMenuStrip1_Opening(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+
+    End Sub
+
+End Class
